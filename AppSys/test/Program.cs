19517379e9978cffadd9c2dfff18de65d54db34d
Allega <42183540+MrsMars@users.noreply.github.com>
2018-09-19 13:12:20 +0300

    add switch to make a choice

--- AppSys/test/Program.cs
+++ AppSys/test/Program.cs
@@ -27,10 +27,46 @@ namespace test
 
             if (choice == 0) { goto l_end; }
 
+l_main:
+            Console.Clear();
+            Console.WriteLine("*** Main ***");
+            Console.WriteLine("Prease, chose what you want to do:\n");
+            Console.WriteLine("Press '1' to see all the students and info about them");
+            Console.WriteLine("Press '2' to see all the universities");
+            Console.WriteLine("Press '3' to find all students on chosen faculty and chosen university");
 
+            Console.Write("\nPlease, choose someone: ");
+
+            do { choice = int.Parse(Console.ReadLine()); }
+            while (choice != 0 & choice != 1 & choice != 2 & choice != 3 & choice != 4 & choice != 5 & choice != 6 & choice != 7 & choice != 8 & choice != 9);
+
+            Console.Clear();
+
+            switch (choice)
+            {
+                case 0: goto l_end;
+                case 1:
+                    ShowAllStudents(xdoc);
+                    break;
+                case 2:
+                    ShowAllUniversities(xdoc);
+                    break;
+                case 3:
+                    FindSometh(xdoc);
+                    break;
+            }
+
+            Console.WriteLine("\nPress '1' to back to the main or '0' to end:");
+
+            do { choice = int.Parse(Console.ReadLine()); }
+            while (choice != 0 & choice != 1);
+
+            if (choice == 1) { goto l_main; }
+            else { goto l_end; }
 l_end:
             return;
         }
+
     }
     class Student
     {
