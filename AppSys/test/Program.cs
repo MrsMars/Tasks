4b60b33891a094119e6bc282e2c3ed3994bddef8
Allega <42183540+MrsMars@users.noreply.github.com>
2018-09-19 13:04:09 +0300

    create program and add class Student

---
+++ AppSys/test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Faculty { get; set; }
+        public string University { get; set; }
+    }
+}
+
